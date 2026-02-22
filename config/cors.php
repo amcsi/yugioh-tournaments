da62aa0629b1eb644e64a8ca4be06e7210b856e6
Attila Szeremi <attila@szeremi.org>
2026-02-22 17:36:17 +0100

    Add CORS

---
+++ config/cors.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+
+return [
+    'allowed_origins' => [
+        'http://yugiohverseny.localhost',
+        'https://yugiohverseny.localhost',
+        'https://yugiohverseny.hu',
+        'http://localhost',
+    ],
+];
